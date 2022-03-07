a31e5aacfca26fcddc1edfb766d9bdf4b6ed57b2
nicolasZhao1908 <0619971117@uma.es>
2022-03-07 12:59:52 +0100

    Create Fibonacci.java and FibonacciTest.java

---
+++ src/main/java/org/zzlnico/fibonacci/Fibonacci.java
@@ -0,0 +1,9 @@
+package org.zzlnico.fibonacci;
+
+public class Fibonacci {
+    /**
+     * Class providing a method that computes the nth fibonacci number given an integer number
+     *
+     * @author Nicolás Zhao
+     */
+}
---
+++ src/test/java/org/zzlnico/fibonacci/FibonacciTest.java
@@ -0,0 +1,7 @@
+package org.zzlnico.fibonacci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciTest {
+
+}

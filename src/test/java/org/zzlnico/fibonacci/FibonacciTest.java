ad6018a77b4f9c77c8d569dbff826620685c1443
nicoZhao1908 <0619971117@uma.es>
2022-03-07 13:40:26 +0100

    Update FibonacciTest class with new test cases

--- src/test/java/org/zzlnico/fibonacci/FibonacciTest.java
+++ src/test/java/org/zzlnico/fibonacci/FibonacciTest.java
@@ -2,6 +2,17 @@ package org.zzlnico.fibonacci;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test cases:
+ * fibonacci 0 -> 0
+ * fibonacci 1 -> 1
+ * fibonacci 2 -> 1
+ * fibonacci 3 -> 2
+ * fibonacci 4 -> 3
+ * fibonacci 5 -> 5
+ * fibonacci 6 -> 8
+ * fibonacci negative number -> raises an exception
+ */
 class FibonacciTest {
 
 }
